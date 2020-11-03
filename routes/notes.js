baf2226a4fb2195e7ef8d71701de6a1f105863de
Ali Madooei <madooei@jhu.edu>
2020-11-03 09:15:00 -0500

    Step 8

--- routes/notes.js
+++ routes/notes.js
@@ -7,11 +7,19 @@ const notes = new NoteDao();
 addSampleNotes(notes);
 
 router.get("/api/notes", (req, res) => {
-
+  const author = req.query.author;
+  notes
+    .readAll(author)
+    .then((notes) => res.json({ data: notes }))
+    .catch((err) => errorHandler(res, 500, err));
 });
 
 router.get("/api/notes/:id", (req, res) => {
-
+  const id = req.params.id;
+  notes
+    .read(id)
+    .then((note) => res.json({ data: note }))
+    .catch((err) => errorHandler(res, 500, err));
 });
 
 router.post("/api/notes", (req, res) => {
@@ -25,5 +33,16 @@ router.delete("/api/notes/:id", (req, res) => {
 router.put("/api/notes/:id", (req, res) => {
 
 });
+
+function errorHandler(res, status, error) {
+  res.status(status).json({
+    errors: [
+      {
+        status: status,
+        detail: error.message || error,
+      },
+    ],
+  });
+}
 
 module.exports = router;
