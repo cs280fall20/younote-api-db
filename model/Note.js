d10f0b6fac0ebf620d91a509a35876da2a01a690
Ali Madooei <madooei@jhu.edu>
2020-11-03 08:33:46 -0500

    Step 1

--- model/Note.js
+++ model/Note.js
@@ -1,8 +1,11 @@
-class Note {
-  constructor(content, author) {
-    this.content = content;
-    this.author = author;
-  }
-}
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+  content: { type: String, required: true },
+  author: { type: String, required: true },
+});
+
+const Note = mongoose.model("Note", NoteSchema);
 
 module.exports = Note;
