a9951519571b8e833bbfedc6857c211c0287a57c
Ali Madooei <madooei@jhu.edu>
2020-11-03 08:53:53 -0500

    Step 4

--- model/NoteDao.js
+++ model/NoteDao.js
@@ -4,16 +4,20 @@ class NoteDao {
   constructor() {
   }
 
-  create(content, author) {
-    return null;
+  async create(content, author) {
+    const note = await Note.create({ content, author });
+    return note;
+  }  
+
+  async readAll(author = "") {
+    const filter = author ? { author } : {};
+    const notes = await Note.find(filter);
+    return notes;
   }
 
-  readAll(author = "") {
-    return null;
-  }
-
-  read(id) {
-    return null;
+  async read(id) {
+    const note = await Note.findById(id);
+    return note;
   }
 
   update(id, content, author) {
